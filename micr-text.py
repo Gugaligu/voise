3b6ece4e828d4089b71d5787d19e9ce8403d6132
Gugaligu <sharanov-2014@mail.ru>
2024-12-30 17:34:41 +0500

    Add files via upload

--- micr-text.py
+++ micr-text.py
@@ -1,10 +1,21 @@
 import speech_recognition as sr
+from pyaudio import PyAudio
+from speech_recognition import Microphone
 
 # Создаём объект Recognizer
 r = sr.Recognizer()
 
 # Определяем источник звука
-mic = sr.Microphone()
+mic = sr.Microphone(1)
+
+p = PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+
 while True:
     with mic as source:
         print("Говорите...")
